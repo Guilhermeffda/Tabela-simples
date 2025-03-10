0440f41cdcee733f6df8d86c8ce6ec5f5b79a366
Guilhermeffda <guiffa05@gmail.com>
2025-03-10 12:14:01 -0300

    Codigo

---
+++ Tabelasimples.py
@@ -0,0 +1,12 @@
+from tabulate import tabulate
+
+dados = [
+    ["nome", "idade", "Profissao"],
+    ["Guilherme", 21, "analista de dados"],
+    ["Joao", 20, "dev front-end"],
+    ["Eduardo", 20, "deb back-end"],
+]
+
+tabela = tabulate(dados, headers="firstrow", tablefmt="grid")
+
+print(tabela)
